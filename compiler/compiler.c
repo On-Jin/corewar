6d7be2e13f955c725c097eb5d5e2db4df592e955
Émile PRÉVOT <mprevot@student.42.fr>
2017-03-08 12:12:49 +0100

    compiler : separate files

--- compiler/compiler.c
+++ compiler/compiler.c
@@ -10,8 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/op.h"
-#include "../includes/libft/libft.h"
+#include "compiler.h"
 
 void error(char *str)
 {
@@ -29,83 +28,6 @@ char *get_output_path(char *name)
 		return (ft_strdup(".cor"));
 	*tmp = '\0';
 	return (ft_strjoin_multi(FALSE, name, ".cor", NULL));
-}
-
-void	write_exec_magic(int fd, header_t *header)
-{
-	int nbr;
-	int revnbr;
-	int i;
-	int size;
-
-	i = 3;
-	nbr = COREWAR_EXEC_MAGIC;
-	revnbr = 0;
-	while (i >= 0)
-	{
-		((char*)(&revnbr))[i] = ((char*)(&nbr))[0];
-		nbr = nbr >> 8;
-		i--;
-	}
-	//write(fd, &revnbr, 4);
-	header->magic = revnbr;
-}
-
-void	write_comment(int fdin, header_t *header)
-{
-	char	*line;
-	char	*str;
-	t_bool 	have_comment;
-	t_bool	have_name;
-	int 	len;
-
-	have_comment = FALSE;
-	have_name = FALSE;
-	while (ft_gnl(fdin, &line))
-	{
-		if (*line == '\0' || ft_strcmp(line, ".") == 0)
-			continue;
-		else if (ft_strncmp(line, NAME_CMD_STRING, ft_strlen(NAME_CMD_STRING)) == 0 && !have_comment)
-		{
-			have_name = TRUE;
-			str = ft_strtrim(line + ft_strlen(NAME_CMD_STRING));
-			len = ft_strlen(str);
-			if (*str != '"')
-				error("Name don't start by '\"'\n");
-			if (str[len - 1] != '"')
-				error("Name don't end by '\"'\n");
-			ft_printf("Hey : %s\n", str);
-			if (len > PROG_NAME_LENGTH)
-				error("Name too long");
-			ft_bzero(&(header->prog_name), PROG_NAME_LENGTH + 1);
-			ft_memcpy(&(header->prog_name), str + 1, len - 2);
-			free(str);
-			if (have_comment && have_name)
-				break;
-		}
-		else if (ft_strncmp(line, COMMENT_CMD_STRING, ft_strlen(COMMENT_CMD_STRING)) == 0 && !have_comment)
-		{
-			have_comment = TRUE;
-			str = ft_strtrim(line + ft_strlen(COMMENT_CMD_STRING));
-			len = ft_strlen(str);
-			if (*str != '"')
-				error("Name don't start by '\"'\n");
-			if (str[len - 1] != '"')
-				error("Name don't end by '\"'\n");
-			ft_printf("Hey : %s\n", str);
-			if (len > COMMENT_LENGTH)
-				error("Name too long");
-			ft_bzero(&(header->comment), COMMENT_LENGTH + 1);
-			ft_memcpy(&(header->comment), str + 1, len - 2);
-			free(str);
-			if (have_comment && have_name)
-				break;
-		}
-		else
-			error("Error in name/description.\n");
-	}
-	if (!have_comment || !have_name)
-		error("Missing comment or name.\n");
 }
 
 int main(int argc, char **argv)

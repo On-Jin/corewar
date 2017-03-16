bfefb6208a775f17d9631bfb1c8c21c55183bfea
Guillaume lale NEBIE <gnebie@e1r5p11.42.fr>
2017-03-16 23:34:29 +0100

    patch vm_put_nbr_in_arene.c

--- VM/srcs/vm_op_mod/vm_put_nbr_in_arene.c
+++ VM/srcs/vm_op_mod/vm_put_nbr_in_arene.c
@@ -17,9 +17,9 @@ void			vm_put_nbr_in_arene(int number, int adress, char *arene, int size)
 	(adress < 0) ? (adress += MEM_SIZE) : 0;
 	while (size)
 	{
-		arene[adress % MEM_SIZE] = (char)((number >> (8 * (4 - size))) & 0xff);
+		arene[adress % MEM_SIZE] = (char)((number >> (8 * 3)));
 		++adress;
-		number >>= 8;
+		number <<= 8;
 		--size;
 	}
 }

b42ffa7cdee4befe49998256edca82e881e106c0
Jino <ntoniolo@student.42.fr>
2017-03-26 00:30:09 +0100

    FULL PATCH ?

--- VM/srcs/vm_op_mod/vm_op_16_exec.c
+++ VM/srcs/vm_op_mod/vm_op_16_exec.c
@@ -6,7 +6,7 @@
 /*   By: gnebie <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/12 14:22:29 by gnebie            #+#    #+#             */
-/*   Updated: 2017/03/22 23:52:30 by ntoniolo         ###   ########.fr       */
+/*   Updated: 2017/03/26 00:17:40 by ntoniolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,15 @@ void			vm_op_16_exec(t_datas *datas, t_process *process)
 {
 	if (!(vm_verif_datas(datas, process)))
 	{
-		process->in_stock[3] = -1;
-		process->PC = (process->PC + 2) % MEM_SIZE;
-		return ;
+//		process->in_stock[3] = -1;
+//		process->PC = (process->PC + 2) % MEM_SIZE;
+//		return ;
 	}
 /*	vm_recup_all_process(process, datas->arene, 0);
 	if (process->in_stock[3] != -1 && (!(datas->flag & FLAG_D) || !(datas->flag & FLAG_N)))
 		ft_putchar(process->in_stock[0] % (1 << 8));
 	if (process->in_stock[3] <= 0)
 		process->in_stock[3] = 3;*/
-	process->PC = (process->PC + 3) % MEM_SIZE;
+	process->PC = vm_op_jump(datas, process,
+								datas->op_tab[(int)process->instruction].nb_arg);
 }
--- VM/srcs/vm_op_mod/vm_recup_indirect_num.c
+++ VM/srcs/vm_op_mod/vm_recup_indirect_num.c
@@ -6,7 +6,7 @@
 /*   By: gnebie <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/12 14:49:56 by gnebie            #+#    #+#             */
-/*   Updated: 2017/03/24 22:48:30 by ntoniolo         ###   ########.fr       */
+/*   Updated: 2017/03/25 23:24:36 by ntoniolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,9 @@
 int				vm_recup_indirect_num(t_process *process, char *arene,
 																	int adresse)
 {
-	int		ind_adress;
+	int			ind_adress;
 	int					val;
+
 	ind_adress = (vm_recup_arena_num(2, arene, adresse));
 	if (process->instruction < 13 || process->instruction > 15)
 		ind_adress = ind_adress % IDX_MOD;
